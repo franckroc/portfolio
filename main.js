c560a19628e5b262a04b41bbba18f2c379ced347
Franck Rochette <rochette.franck@gmail.com>
2022-11-10 19:53:36 +0100

    modif portfolio

--- main.js
+++ main.js
@@ -59,41 +59,9 @@ function mailing() {
         }
     }
 
-// canvas anim1 
-var monCanvas = document.getElementById('anim1');
-if (monCanvas.getContext){
-    var ctext = monCanvas.getContext('2d');
-    // couleur rectangle
-    var gradient = ctext.createRadialGradient(450, 80, 50, 450, 85, 2*Math.PI);
-    gradient.addColorStop(0, '#ffa500');
-    gradient.addColorStop(1, '#ffffff');
-    ctext.fillStyle = gradient;
-    // translation au centre du contexte
-    ctext.translate(monCanvas.width/2,monCanvas.height/2)
-    // incrément
-    var i = 0;
-    var a = 0;
-    // timer
-    var inter = setInterval(Truc,1);    
-} else {
-    alert('canvas non supporté par ce navigateur');
-}
+    function animation1() {
+        for (let i = 0; i < 20; i++) {
+            
+        }
 
-//dessin 
-function Truc() {
-    ctext.lineWidth = 1;
-    ctext.beginPath();
-    a--;
-    //ctext.clearRect(0, 0, monCanvas.width,monCanvas.height);
-    // arc : x, y, radius, startAngle, endAngle, antiClockwise 
-    ctext.arc(a,a, 5, 0, 2 * Math.PI, false);
-    ctext.fill();
-    
-    ctext.rotate(0.5);
-    
-    //ctext.arc(monCanvas.width/2,monCanvas.height/2, 10, 0, 2 * Math.PI, false);
-    //ctext.fillRect(0, 0, 200, 20); 
-
-    
-    
-}
+    }
